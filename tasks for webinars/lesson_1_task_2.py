c2212c39566ca06a2cb9b415a4143b350f707e23
Dmitry Ageev <ageev.dmitry.s@gmail.com>
2022-08-02 22:53:30 +0300

    Complete task 1, 2. Add task 3, 4

--- tasks for webinars/lesson_1_task_2.py
+++ tasks for webinars/lesson_1_task_2.py
@@ -3,3 +3,11 @@
 Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
 Используйте форматирование строк.
 """
+
+input_time = int(input('Enter time in seconds: '))
+
+hours = input_time // 3600
+minutes = input_time % 3600 // 60
+seconds = input_time % 3600 % 60
+
+print(f'Your time: {hours:02}:{minutes:02}:{seconds:02}')
